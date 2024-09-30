659758971d7fdfb25aeab40577abcc2e619b03f5
Razvan-Simion <158332391+Razvan-Simion@users.noreply.github.com>
2024-10-01 01:08:28 +0300

    First exercise

---
+++ js/task-01.js
@@ -0,0 +1,8 @@
+const menuItemsByClass = document.querySelectorAll(".item");
+
+console.log(`Number of categories: ${menuItemsByClass.length}`);
+
+const elements = menuItemsByClass.forEach(li => {
+    console.log(`Category: ${li.children[0].innerHTML} 
+Elements: ${li.children[1].children.length}`)
+});
